90fade438d641ec144aa06852213fcae00a7d0a9
Cameron von Criegern <cameronvoncriegern@trentu.ca>
2024-07-16 19:34:49 -0400

    fixed cors and stuff

--- assignment_3/src/components/MovieCard.jsx
+++ assignment_3/src/components/MovieCard.jsx
@@ -1,13 +1,14 @@
 import "../styles/MovieCard.css";
+import { Link } from "react-router-dom";
 
 function MovieCard({ movie }) {
   return (
     <>
       <div className="MovieCard">
-        <h1>Card</h1>
         <h3>{movie.title}</h3>
         <img src={movie.poster} />
         <p>{movie.vote_average}</p>
+        <Link to={`/movie/${movie.movieid}`}>More Info</Link>
       </div>
     </>
   );
--- assignment_3/src/components/routes/Home.jsx
+++ assignment_3/src/components/routes/Home.jsx
@@ -4,23 +4,19 @@ import { useState, useEffect } from "react";
 function Home() {
   const [MovieList, setMovieList] = useState([]);
   let url =
-    // "https://loki.trentu.ca/~cameronvoncriegern/3430/assn/assignment2/api/movies";
-    "https://loki.trentu.ca/~molayoogunfowora/3430/assn/cois3430-2024su-a2-Molayo-0/api/movies/";
+    "https://loki.trentu.ca/~cameronvoncriegern/3430/assn/assignment2/api/movies";
+  // "https://loki.trentu.ca/~molayoogunfowora/3430/assn/cois3430-2024su-a2-Molayo-0/api/movies/";
+
+  async function fetchMovies() {
+    const resp = await fetch(url);
+    const jsonResponse = await resp.json();
+    const movies = jsonResponse;
+    setMovieList(movies);
+  }
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        console.log("Response:", response);
-        return response.json();
-      })
-      .then((json) => {
-        console.log("JSON:", json);
-        setMovieList(json);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, [url]);
+    fetchMovies();
+  }, []);
 
   return (
     <>

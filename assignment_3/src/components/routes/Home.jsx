cf0ed5da62734e0b0e91ebd7dcb24b49141b0445
Cameron von Criegern <cameronvoncriegern@trentu.ca>
2024-07-16 19:32:54 -0400

    fixed CORS and implemented movie grid and movies/ID  ~ish

--- assignment_3/src/components/routes/Home.jsx
+++ assignment_3/src/components/routes/Home.jsx
@@ -4,13 +4,22 @@ import { useState, useEffect } from "react";
 function Home() {
   const [MovieList, setMovieList] = useState([]);
   let url =
-    "https://loki.trentu.ca/~cameronvoncriegern/3430/assn/assignment2/api/movies";
-  // "https://loki.trentu.ca/~molayoogunfowora/3430/assn/cois3430-2024su-a2-Molayo-0/api/movies/";
+    // "https://loki.trentu.ca/~cameronvoncriegern/3430/assn/assignment2/api/movies";
+    "https://loki.trentu.ca/~molayoogunfowora/3430/assn/cois3430-2024su-a2-Molayo-0/api/movies/";
 
   useEffect(() => {
     fetch(url)
-      .then((response) => response.json())
-      .then((json) => setMovieList(json));
+      .then((response) => {
+        console.log("Response:", response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log("JSON:", json);
+        setMovieList(json);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
   }, [url]);
 
   return (

992791e00d25cf589f478dc504a83c147d92d454
Molayo Ogunfowora <molayoogunfowora@trentu.ca>
2024-07-20 15:41:21 -0400

    Add a private route for users to visit sites that need authentication

--- assignment_3/src/components/routes/authRoute.jsx
+++ assignment_3/src/components/routes/authRoute.jsx
@@ -1,13 +1,13 @@
 //This component simply checks if the user is authenticated
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from "../Authentication";
 
 const AuthRoute = () => {
-  //Access isAuth value from AuthContext global state
+  //Access isAuth value and API key from AuthContext global state
   const { isAuth } = useContext(AuthContext);
   //determine if isAuth == True. if so render children component, else redirect to login page
-  return isAuth ? <Outlet /> : <Navigate to="/login" />;
+  return isAuth ? <Outlet /> : <Navigate to="/Login" />;
 };
 
 export default AuthRoute;

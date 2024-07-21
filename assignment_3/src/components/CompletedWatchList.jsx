a10edf71a1447e5ae898f8dc97f3bcb097a5ddc1
Molayo Ogunfowora <molayoogunfowora@trentu.ca>
2024-07-21 13:20:39 -0400

    Add function to update times watched and score

--- assignment_3/src/components/CompletedWatchList.jsx
+++ assignment_3/src/components/CompletedWatchList.jsx
@@ -9,21 +9,55 @@ export default function CompletedWatchlist() {
   //initalize state variables
   const [List, setMovieList] = useState([]);
   const { apiKey } = useContext(AuthContext);
+  const [score, setScore] = useState(0);
 
   //define function to fetch all completed movies
   async function fetchCompletedMovies() {
     let baseUrl =
       "https://loki.trentu.ca/~molayoogunfowora/3430/assn/cois-3430-2024su-a2-Molayo-0/api/completedwatchlist/entries";
-    const resp = await fetch(baseUrl, { headers: { "X-API-Key": apiKey } });
+    const resp = await fetch(baseUrl, {
+      headers: { "X-API-Key": apiKey },
+    });
     const jsonResponse = await resp.json();
     const movies = jsonResponse;
     setMovieList(movies);
     console.log(movies);
   }
 
+  //define function to update movie properties
+  async function updateMovieDetail(movieId, endpoint, newValue) {
+    const baseUrl = `https://loki.trentu.ca/~molayoogunfowora/3430/assn/cois-3430-2024su-a2-Molayo-0/api/completedwatchlist/entries/${movieId}/${endpoint}`;
+
+    const resp = await fetch(baseUrl, {
+      method: "PATCH",
+      headers: {
+        "X-API-Key": apiKey,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ [endpoint]: newValue }),
+    });
+
+    const response = await resp.json();
+    // If the response is successful, update the movieList with the updated value
+    if (response.status >= 200 && response.status < 300) {
+      // Iterate to update the corresponding movieID
+      const newMovieList = List.map((movie) => {
+        if (movie.movieID === movieId) {
+          return { ...movie, [endpoint]: newValue };
+        } else {
+          return movie;
+        }
+      });
+      setMovieList(newMovieList); //Update state variable
+    } else {
+      console.error("Failed to update movie detail:", response);
+    }
+  }
+
   useEffect(() => {
     fetchCompletedMovies();
   }, []);
+
   return (
     <Fragment>
       <h1 className="Heading">Completed WatchList</h1>
@@ -53,8 +87,37 @@ export default function CompletedWatchlist() {
                 </div>
               </td>
               <td>{movie.Release_Date}</td>
-              <td>{movie.rating}</td>
-              <td>{movie.Times_watched}</td>
+              <td>
+                {movie.rating}
+                <form
+                  className="scoreForm"
+                  onSubmit={(event) => {
+                    event.preventDefault(); // Prevent form submission
+                    updateMovieDetail(movie.movieID, "rating", score); // Pass the score value to the updateMovieDetail function
+                  }}
+                >
+                  <input
+                    type="number"
+                    id="score"
+                    value={score}
+                    onChange={(event) => setScore(event.target.value)} // Update the score state based on user input
+                  />
+                </form>
+              </td>
+              <td>
+                {movie.Times_watched}
+                <button
+                  onClick={() =>
+                    updateMovieDetail(
+                      movie.movieID,
+                      "times-watched",
+                      movie.Times_watched + 1
+                    )
+                  }
+                >
+                  Increase
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>

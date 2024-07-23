ee3c6dc83949e977a757d19f63235d1ddaa92f2b
Molayo Ogunfowora <molayoogunfowora@trentu.ca>
2024-07-23 13:03:55 -0400

    restructure code to add movie row component

--- assignment_3/src/components/CompletedWatchList.jsx
+++ assignment_3/src/components/CompletedWatchList.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { AuthContext } from "./Authentication";
-import MovieGrid from "./MovieGrid";
+import MovieRow from "./completedMovieRow";
 import { Link } from "react-router-dom";
 import { Fragment } from "react";
 import "../styles/completedWatch.css";
@@ -9,27 +9,29 @@ export default function CompletedWatchlist() {
   //initalize state variables
   const [List, setMovieList] = useState([]);
   const { apiKey } = useContext(AuthContext);
-  const [score, setScore] = useState(0);
-
+  
   //define function to fetch all completed movies
   async function fetchCompletedMovies() {
     let baseUrl =
-      "https://loki.trentu.ca/~molayoogunfowora/3430/assn/cois-3430-2024su-a2-Molayo-0/api/completedwatchlist/entries";
+    "https://loki.trentu.ca/~molayoogunfowora/3430/assn/cois-3430-2024su-a2-Molayo-0/api/completedwatchlist/entries";
     const resp = await fetch(baseUrl, {
       headers: { "X-API-Key": apiKey },
     });
     const jsonResponse = await resp.json();
     const movies = jsonResponse;
     setMovieList(movies);
-    console.log(movies);
   }
+  
+  useEffect(() => {
+    fetchCompletedMovies();
+  }, []);
 
   //define function to update movie properties
   async function updateMovieDetail(movieId, endpoint, newValue) {
     const baseUrl = `https://loki.trentu.ca/~molayoogunfowora/3430/assn/cois-3430-2024su-a2-Molayo-0/api/completedwatchlist/entries/${movieId}/${endpoint}`;
 
     const formData = new URLSearchParams();
-    formData.append("rating", newValue);
+    formData.append(endpoint, newValue);
     const resp = await fetch(baseUrl, {
       method: "PATCH",
       headers: {
@@ -54,9 +56,6 @@ export default function CompletedWatchlist() {
     }
   }
 
-  useEffect(() => {
-    fetchCompletedMovies();
-  }, []);
 
   return (
     <Fragment>
@@ -75,51 +74,11 @@ export default function CompletedWatchlist() {
         </thead>
         <tbody>
           {List.map((movie) => (
-            <tr key={movie.movieID}>
-              <td>{movie.Initially_Watched}</td>
-              <td>{movie.Last_Watched}</td>
-              <td>
-                <div className="movie-container">
-                  <img src={movie.Poster} alt="movie Poster" />
-                  <Link to={`/movie/${movie.movieID}`}>
-                    <h4>{movie.Title}</h4>
-                  </Link>
-                </div>
-              </td>
-              <td>{movie.Release_Date}</td>
-              <td className="display-rating">
-                {movie.rating}
-                <form
-                  className="rating-form"
-                  onSubmit={(event) => {
-                    event.preventDefault(); // Prevent form submission
-                    updateMovieDetail(movie.movieID, "rating", score); // Pass the score value to the updateMovieDetail function
-                  }}
-                >
-                  <input className="rating-input"
-                    type="number"
-                    //Add unique id for each input field to avoid updating all movie ratings
-                    id={`score for movie ${movie.movieID}`} 
-                    value={score}
-                    onChange={(event) => setScore(event.target.value)} // Update the score state based on user input
-                  />
-                </form>
-              </td>
-              <td className="display-side">
-                {movie.Times_watched}
-                <button
-                  onClick={() =>
-                    updateMovieDetail(
-                      movie.movieID,
-                      "times-watched",
-                      movie.Times_watched + 1
-                    )
-                  }
-                >
-                  +
-                </button>
-              </td>
-            </tr>
+            <MovieRow
+              key={movie.movieID}
+              movie={movie}
+              updateMovieDetail={updateMovieDetail}
+            />
           ))}
         </tbody>
       </table>

7e1ff121fe824badb0fbe191500b463dac7365e6
Molayo Ogunfowora <molayoogunfowora@trentu.ca>
2024-08-12 23:18:01 -0400

    Restructure Nav Links

--- Movie-React/src/components/Nav.jsx
+++ Movie-React/src/components/Nav.jsx
@@ -1,6 +1,17 @@
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./Authentication";
 
 function Nav() {
+  //Add auth Context
+  const { isAuth, setIsAuth, setApiKey } = useContext(AuthContext);
+
+  //function to handle logout
+  const handleLogout = () => {
+    setIsAuth(false);
+    setApiKey(null);
+  };
+
   return (
     <>
       <nav className="Navbar">
@@ -8,19 +19,41 @@ function Nav() {
           <h1>Watched It</h1>
         </NavLink>
 
+        {/* Conditionally render links based on Auth */}
+
         <ul className="Navlinks">
           <li>
-            <NavLink to="/Login">Log In</NavLink>
+            <NavLink to="/">Films</NavLink>
           </li>
-          <li>
-            <NavLink to="/Signup">Sign Up</NavLink>
-          </li>
-          <li>
-            <NavLink to ="/MyAccount/Watchlist">ToWatch</NavLink>
-          </li>
-          <li>
-            <NavLink to ="/MyAccount/CompletedWatchList">Logs</NavLink>
-          </li>
+          {isAuth ? (
+            <>
+              <li>
+                <NavLink to="/MyAccount/UserStats">MyAccount</NavLink>
+              </li>
+              <li>
+                <NavLink to="/MyAccount/Watchlist">Watchlist</NavLink>
+              </li>
+              <li>
+                <NavLink to="/MyAccount/CompletedWatchList">
+                  Film Journal
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/Login" onClick={handleLogout}>
+                  Log Out
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/Login">Log In</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Signup">Sign Up</NavLink>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </>
